a3eb7eef8bac0dc61f54485e2858bf7e44d32836
Informatievoorzieningendeltionflex435 <97119326@st.deltion.nl>
2026-02-05 16:12:09 +0100

    nu

--- app/030_berekeningen/opdr_1/opdr_1.py
+++ app/030_berekeningen/opdr_1/opdr_1.py
@@ -3,6 +3,22 @@
 # Groep:
 
 # Hier komt je code...
-print("Dit weten we over joe, bob en fred: ")  
-print("De totale waarde van alle leeftijden bij elkaar opgeteld = 60")  
-print("De gemiddelde leeftijd van deze boys is: 20")
+
+joe = int("30")
+bob = int("23")
+fred = int("32")
+totaal = joe + bob + fred
+gemiddelde = totaal / 3
+print(" ")
+print(" ")
+print("Dit weten we over joe, bob en fred: Joe is 30, Bob is 23 en Fred is 32")
+print(" ")
+print(" ")
+print("De totale waarde van alle leeftijden bij elkaar opgeteld is,"), print(totaal)
+print(" ")
+print(" ")
+print("De gemiddelde leeftijd van deze boys is:,"),
+print(gemiddelde)
+print(" ")
+print(" ")
+print(" ")
