340660422777ecb529fc59cadc5440b0d7faedc5
Informatievoorzieningendeltionflex435 <97119326@st.deltion.nl>
2026-02-05 16:02:54 +0100

    l222323

--- app/030_berekeningen/opdr_1/opdr_1.py
+++ app/030_berekeningen/opdr_1/opdr_1.py
@@ -3,3 +3,6 @@
 # Groep:
 
 # Hier komt je code...
+print("Dit weten we over joe, bob en fred: ")  
+print("De totale waarde van alle leeftijden bij elkaar opgeteld = 60")  
+print("De gemiddelde leeftijd van deze boys is: 20")
