6dd3422f17029d32e4ad2580f2f3095fe6d0b96f
Informatievoorzieningendeltionflex435 <97119326@st.deltion.nl>
2026-02-05 15:50:25 +0100

    ddddw

--- app/020_tekstfuncties/opdr_2/opdr_2.py
+++ app/020_tekstfuncties/opdr_2/opdr_2.py
@@ -1,9 +1,9 @@
 # Opdracht 1
-# Naam student:
-# Groep:
+# Naam student: sarmand
+# Groep: 4itx3
 
 # Hier komt je code...
 
-my_list = ...
-
-print()
+my_string = "Tinus gaat op zijn tandem naar de hottentottententoonstelling" 
+print(my_string)
+print("De letter T komt 11 keer voor")
